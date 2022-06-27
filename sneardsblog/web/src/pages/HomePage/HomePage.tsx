4ab18ad4badba803c4e7c1bc8de849a909b79319
Snorre Søvold <snorresovold@gmail.com>
2022-06-27 20:05:34 +0200

    installed tailwind

--- sneardsblog/web/src/pages/HomePage/HomePage.tsx
+++ sneardsblog/web/src/pages/HomePage/HomePage.tsx
@@ -6,7 +6,7 @@ const HomePage = () => {
     <>
       <MetaTags title="Home" description="Home page" />
 
-      <h1>HomePage</h1>
+      <h1 className='text-3xl font-bold underline'>HomePage</h1>
       <p>
         Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
       </p>
